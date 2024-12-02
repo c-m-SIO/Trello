7dfa7d2f8e3f6379b4e44610a538bf9e0d3a67a7
Antonin Brochard <antonin.brochard@gmail.com>
2024-12-02 16:28:15 +0100

    crud

---
+++ src/Form/ProjetsType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Projets;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjetsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('idProjet')
+            ->add('titre')
+            ->add('dateCreation', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('dateFin', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('description')
+            ->add('image')
+            ->add('users', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Projets::class,
+        ]);
+    }
+}
